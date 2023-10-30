7fb057ed9a165ac04b17b2e62f5a22a0933a403e
martinez415 <gigimartinez415@gmail.com>
2023-10-30 12:56:34 -0500

    Declaring Initializing Arrays

---
+++ ArraysCodingSample/SalesArrayApp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Decalre two arrays: 1D string with names of each store and 2D with sales amount for each store.
+    // When you declare any numerical value dont assume it is currently 0. At first they are null.  
+    string store[5] = { 0 };
+    double MonthlySales[5][4] = {0};
+
+    
+}
+
+
