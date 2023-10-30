69ac3b3453ee8f08509e76760dc660bf15a407ec
martinez415 <gigimartinez415@gmail.com>
2023-10-30 14:20:35 -0500

    Display monthly sales reports

--- ArraysCodingSample/SalesArrayApp.cpp
+++ ArraysCodingSample/SalesArrayApp.cpp
@@ -1,14 +1,45 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
+#include <ios>
 using namespace std;
 
 int main()
 {
-    // Decalre two arrays: 1D string with names of each store and 2D with sales amount for each store.
-    // When you declare any numerical value dont assume it is currently 0. At first they are null.  
-    string store[5] = { 0 };
-    double MonthlySales[5][4] = {0};
+    string store[5];
+    double MonthlySales[5][5] = {0};
 
+    int lengthC = sizeof(MonthlySales[0]) / sizeof(double);
+
+    int lengthR = (sizeof(MonthlySales) / sizeof(double)) / lengthC;
+
+    cout << "Monthly Sales Application\n=========================" << endl;
+    for (int r = 0; r < lengthR; r++) {
+
+        cout << "Please enter the store #" << (r+1) << " name: ";
+        getline(cin, store[r]);
+        
+        for (int c = 0; c < lengthC - 1; c++) {
+            cout << "Please enter sales for " << store[r] << " store month #" << (c+1) << ": ";
+            cin >> MonthlySales[r][c];
+            MonthlySales[r][lengthC - 1] += MonthlySales[r][c];
+        }
+        cin.ignore(); 
+    }
+
+    cout << "Monthly Sales Report\n====================" << endl;
+    for (int r = 0; r < lengthR; r++) {
+
+        cout << store[r] <<  " Monthly Sales: ";
+
+        for (int c = 0; c < lengthC - 1; c++) {
+
+            cout << MonthlySales[r][c] << setw(5);
+
+        }
+        cout << " Total Sales: " << MonthlySales[r][lengthC - 1];
+        cout << endl;
+    }
     
 }
 
